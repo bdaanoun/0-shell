7bcf19b6ee0aeb5eccf5c47a08e44c07811d495a
achbarou <achbarou@192.168.1.87>
2025-10-20 16:14:49 +0100

    impl echo cmd

--- shell0/src/commands/echo.rs
+++ shell0/src/commands/echo.rs
@@ -1,4 +1,31 @@
+ use std::io;
+  use std::io::Write;
 pub fn echo(args: &[&str]) -> Result<(), String> {
-    println!("{}", args.join(" "));
+    let mut raw_string = args.join(" ");
+
+    if raw_string.matches('"').count() % 2 != 0 {
+        loop {
+            print!("> ");
+            io::stdout().flush().unwrap(); 
+
+            let mut continuation_line = String::new();
+            match io::stdin().read_line(&mut continuation_line) {
+                Ok(0) => break, 
+                Ok(_) => {
+                    raw_string.push_str(&continuation_line);
+                    if raw_string.matches('"').count() % 2 == 0 {
+                        break;
+                    }
+                }
+                Err(e) => {
+                    return Err(format!("Error reading input: {}", e));
+                }
+            }
+        }
+    }
+
+    let output = raw_string.replace('"', "");
+    println!("{}", output);
+    
     Ok(())
-}
+} 
