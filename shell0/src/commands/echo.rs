928fe1288b8d1484ee23aee158602b4034fd6c44
aachbarou <ahmedachbarou842@gmail.com>
2025-10-21 15:07:05 +0100

    feat | impl qoute in echo  cmd

--- shell0/src/commands/echo.rs
+++ shell0/src/commands/echo.rs
@@ -3,24 +3,32 @@ use std::io::Write;
 
 pub fn echo(args: &[&str]) -> Result<(), String> {
     let mut raw_string = args.join(" ");
-     let  mut  in_dqoute_loop =  false  ; 
+     let  mut  in_qoute_loop =  false  ;  let  mut  in_dqoute_loop =  false  ; 
 
-    if raw_string.matches('"').count() % 2 != 0 {
+    if (raw_string.matches('"').count() % 2 != 0) || (raw_string.matches("'").count() % 2 != 0) {
         
         if raw_string.starts_with('"') && raw_string.matches('"').count() == 1 {
             in_dqoute_loop = true ;
             raw_string.push('\n');
+        }else if  raw_string.starts_with("'") && raw_string.matches("'").count() == 1 {
+            in_qoute_loop = true ;
+            raw_string.push('\n');
         }
         loop {
-            print!("dqoute> ");
+            if  in_dqoute_loop  {
+                print!("dquote> ");
+            } else if  in_qoute_loop  {
+                print!("quote> ");
+            }
             io::stdout().flush().unwrap(); 
-
             let mut continuation_line = String::new();
             match io::stdin().read_line(&mut continuation_line) {
                 Ok(0) => break, 
                 Ok(_) => {
                     raw_string.push_str(&continuation_line);
-                    if raw_string.matches('"').count() % 2 == 0 {
+                    if raw_string.matches('"').count() % 2 == 0  && in_dqoute_loop {
+                        break;
+                    }else if raw_string.matches('\'').count() % 2 == 0  && in_qoute_loop {
                         break;
                     }
                 }
@@ -31,7 +39,7 @@ pub fn echo(args: &[&str]) -> Result<(), String> {
         }
          
     }
-   if in_dqoute_loop && raw_string.ends_with('\n') {
+   if (in_qoute_loop && raw_string.ends_with('\n')) || (in_dqoute_loop && raw_string.ends_with('\n')) {
         raw_string.pop();
         }
     let output = raw_string.replace('"', "");
