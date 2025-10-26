b66ba555b24f7a9b76508a712d7e77034ce7464d
ahmed achbarou <ahmedachbarou842@gmail.com>
2025-10-26 16:19:56 +0100

    Fixed

--- shell0/target/debug/deps/log-06404c1123fbcf97.d
+++
@@ -1,8 +0,0 @@
-/home/bdaanoun/0-shell/shell0/target/debug/deps/log-06404c1123fbcf97.d: /home/bdaanoun/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/log-0.4.28/src/lib.rs /home/bdaanoun/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/log-0.4.28/src/macros.rs /home/bdaanoun/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/log-0.4.28/src/serde.rs /home/bdaanoun/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/log-0.4.28/src/__private_api.rs
-
-/home/bdaanoun/0-shell/shell0/target/debug/deps/liblog-06404c1123fbcf97.rmeta: /home/bdaanoun/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/log-0.4.28/src/lib.rs /home/bdaanoun/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/log-0.4.28/src/macros.rs /home/bdaanoun/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/log-0.4.28/src/serde.rs /home/bdaanoun/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/log-0.4.28/src/__private_api.rs
-
-/home/bdaanoun/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/log-0.4.28/src/lib.rs:
-/home/bdaanoun/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/log-0.4.28/src/macros.rs:
-/home/bdaanoun/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/log-0.4.28/src/serde.rs:
-/home/bdaanoun/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/log-0.4.28/src/__private_api.rs:
